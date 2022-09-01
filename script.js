25a0b349c7bbb38151930bd58a23b361ebb1578c
Harsha-kalava <harshcode16@gmail.com>
2022-09-01 22:30:49 +0530

    Web Task 4

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ const header = document.getElementById("main-header");
 const headerTitle = document.getElementById("header-title");
 const addItems = document.getElementsByClassName("title");
 let listItems = document.getElementsByClassName("list-group-item");
-
-listItems[2].style.backgroundColor = "green";
+let taglistItems = document.getElementsByTagName("li");
 
 for (i = 0; i < listItems.length; i++) {
 	listItems[i].style.fontWeight = "bold";
+}
+
+for (i = 0; i < taglistItems.length; i++) {
+	taglistItems[i].style.backgroundColor = "#f4f4f4";
 }
