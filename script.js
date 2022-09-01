fe4b2ddf2b64a69de08107d129e555149d2fc831
Harsha-kalava <harshcode16@gmail.com>
2022-09-01 22:01:31 +0530

    Web Task3

---
+++ script.js
@@ -0,0 +1,10 @@
+const header = document.getElementById("main-header");
+const headerTitle = document.getElementById("header-title");
+const addItems = document.getElementsByClassName("title");
+
+header.style.borderBottom = "solid 3px #000";
+console.log(addItems);
+
+// addItems[0].innerHTML = "<h1>Add Items</h3>";
+addItems[0].style.color = "green";
+addItems[0].style.fontWeight = "bold";
